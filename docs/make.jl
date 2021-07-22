864f42474d3c9061f014ec0d1e543f4de7ac1dab
Adham Mohamed-Saber <adham4444@gmail.com>
2021-07-22 15:38:23 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,6 @@ makedocs(build   = "build",
 deploydocs(
     repo = "github.com/erathorn/MCPhyloTree.jl.git",
     versions = ["stable" => "v^", "v#.#.#"],
-    devurl = "test",
+    devbranch = "v0.9.2",
     target = "build"
      )
