72be2e82bf56d80a7b8f960bac3b78dbccaf1e9c
erathorn <erathorn@users.noreply.github.com>
2021-07-26 09:42:00 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,9 @@
 using Documenter, MCPhyloTree
 
-makedocs(build   = "build",
-    clean   = true,
-    doctest = true,
+makedocs(
     modules = Module[MCPhyloTree],
     sitename="MCPhyloTree",
-    format = Documenter.HTML(
-        canonical = "https://erathorn.github.io/MCPhyloTree.jl/dev/"
-        ),
+    format = Documenter.HTML(),
     pages = [
         "Index" => "index.md",
         "Basics" => "basics.md",
@@ -20,9 +16,8 @@ makedocs(build   = "build",
 
 deploydocs(
     repo = "github.com/erathorn/MCPhyloTree.jl.git",
-    target = "build",
-    deps = nothing,
-    make = nothing,
-    devurl = "dev",
-    versions = ["stable" => "v^", "v#.#", devurl => devurl]
+    #target = "build",
+    #deps = nothing,
+    #make = nothing,
+    #versions = ["stable" => "v^", "v#.#", devurl => devurl]
      )
