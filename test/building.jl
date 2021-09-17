4e42afe2b92a4225c4cef984d7561802ca636533
eratorn <wahle.joh@gmail.com>
2021-09-17 09:09:18 +0200

    test leave incidence function

--- test/building.jl
+++ test/building.jl
@@ -1,6 +1,12 @@
 @testset "upgma" begin
-    D = [0. 17. 21. 31. 23. ; 17. 0. 30. 34. 21.; 21. 30. 0. 28. 39.; 31. 34. 28. 0. 43.; 23. 21. 39. 43. 0.]
-    rt = upgma(D, ["a","b","c","d","e"])
+    D = [
+        0.0 17.0 21.0 31.0 23.0
+        17.0 0.0 30.0 34.0 21.0
+        21.0 30.0 0.0 28.0 39.0
+        31.0 34.0 28.0 0.0 43.0
+        23.0 21.0 39.0 43.0 0.0
+    ]
+    rt = upgma(D, ["a", "b", "c", "d", "e"])
     rt2 = upgma(D)
     gst = parsing_newick_string("(((a,b)u,e)v,(c,d)w)r;")
     MCPhyloTree.number_nodes!(gst)
@@ -12,8 +18,8 @@
 end
 
 @testset "NJ" begin
-    D = [0. 3. 14. 12.; 3 0 13 11; 14. 13. 0. 4; 12. 11. 4. 0]
-    rt = neighbor_joining(D,["a","b","d","e"])
+    D = [0.0 3.0 14.0 12.0; 3 0 13 11; 14.0 13.0 0.0 4; 12.0 11.0 4.0 0]
+    rt = neighbor_joining(D, ["a", "b", "d", "e"])
     gst = parsing_newick_string("((a,b)c,d,e);")
     MCPhyloTree.number_nodes!(gst)
     MCPhyloTree.set_binary!(gst)
@@ -30,26 +36,30 @@ end
     tree = parsing_newick_string("((B:5,A:5)C:9,(D:5,E:5)F:5)G:5;")
     MCPhyloTree.set_binary!(tree)
     MCPhyloTree.number_nodes!(tree)
-    @test to_covariance(tree) == [14.0 9.0 0.0 0.0; 9.0 14.0 0.0 0.0; 0.0 0.0 10.0 5.0; 0.0 0.0 5.0 10.0]
+    @test to_covariance(tree) ==
+          [14.0 9.0 0.0 0.0; 9.0 14.0 0.0 0.0; 0.0 0.0 10.0 5.0; 0.0 0.0 5.0 10.0]
 end
 
 @testset "ladderize" begin
     tree = parsing_newick_string("((B:5,A:5,X,Y,Z)C:9,(D:5,E:5)F:5)G:5;")
-    ladder = ladderize_tree(tree,true)
-    @test newick(ladder) == "((D:5.0,E:5.0)F:5.0,(B:5.0,A:5.0,X:1.0,Y:1.0,Z:1.0)C:9.0)G:5.0;"
+    ladder = ladderize_tree(tree, true)
+    @test newick(ladder) ==
+          "((D:5.0,E:5.0)F:5.0,(B:5.0,A:5.0,X:1.0,Y:1.0,Z:1.0)C:9.0)G:5.0;"
 end
 
 @testset "to_df" begin
     tree = parsing_newick_string("((B:5,A:5)C:9,(D:5,E:5)F:5)G:5;")
     MCPhyloTree.set_binary!(tree)
     MCPhyloTree.number_nodes!(tree)
-    target_arr = [ 0.0  0.0  0.0  0.0  0.0  0.0  0.0;
-        0.0  0.0  0.0  0.0  0.0  0.0  0.0;
-        5.0  5.0  0.0  0.0  0.0  0.0  0.0;
-        0.0  0.0  0.0  0.0  0.0  0.0  0.0;
-        0.0  0.0  0.0  0.0  0.0  0.0  0.0;
-        0.0  0.0  0.0  5.0  5.0  0.0  0.0;
-        0.0  0.0  9.0  0.0  0.0  5.0  0.0]
+    target_arr = [
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        5.0 5.0 0.0 0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 5.0 5.0 0.0 0.0
+        0.0 0.0 9.0 0.0 0.0 5.0 0.0
+    ]
     target_names = ["B", "A", "C", "D", "E", "F", "G"]
     res_arr, res_names = to_df(tree)
     @test all(target_arr .== res_arr)
@@ -61,14 +71,31 @@ end
     tree = parsing_newick_string("((B:5,A:5)C:9,(D:5,E:5)F:5)G:5;")
     MCPhyloTree.set_binary!(tree)
     MCPhyloTree.number_nodes!(tree)
-    target_arr = [ 0.0  0.0  0.0  0.0  0.0  0.0  0.0;
-        0.0  0.0  0.0  0.0  0.0  0.0  0.0;
-        5.0  5.0  0.0  0.0  0.0  0.0  0.0;
-        0.0  0.0  0.0  0.0  0.0  0.0  0.0;
-        0.0  0.0  0.0  0.0  0.0  0.0  0.0;
-        0.0  0.0  0.0  5.0  5.0  0.0  0.0;
-        0.0  0.0  9.0  0.0  0.0  5.0  0.0]
+    target_arr = [
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        5.0 5.0 0.0 0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 5.0 5.0 0.0 0.0
+        0.0 0.0 9.0 0.0 0.0 5.0 0.0
+    ]
     target_names = ["B", "A", "C", "D", "E", "F", "G"]
     res_tree = from_df(target_arr, target_names)
     @test RF(res_tree, tree) == 0
+end
+
+
+@testset "leave_incidence_matrix" begin
+    tree = parsing_newick_string("((B:5,A:5)C:9,(D:5,E:5)F:5)G:5;")
+    MCPhyloTree.set_binary!(tree)
+    MCPhyloTree.number_nodes!(tree)
+    lm = leave_incidence_matrix(tree)
+    target = [
+        0.0 1.0 0.0 0.0 1.0 0.0
+        1.0 0.0 0.0 0.0 1.0 0.0
+        0.0 0.0 1.0 0.0 0.0 1.0
+        0.0 0.0 0.0 1.0 0.0 1.0
+    ]
+    @test all(lm .== target)
 end
