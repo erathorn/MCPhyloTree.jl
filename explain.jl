de0be2459b4bf146a2666d18a53ab345ccdf3e75
Arcumenn <steinar.graessel@gmx.net>
2021-11-26 16:10:09 +0100

    test file changes

--- explain.jl
+++ explain.jl
@@ -3,9 +3,15 @@ using Pkg
 Pkg.activate(".")
 using MCPhyloTree
 
-trees = ParseNewick("./Drav_mytrees_1.nwk")
+# trees = ParseNewick("Drav_mytrees_1.nwk")
+trees = ParseNewick("gs_tree_JC_20-60-letters.nwk")
 
-t1 = deepycopy(trees[1])
-t2 = deepcopy(trees[3500])
 
-common_nodes = MCPhyloTree.splitOnCommonEdge(t1, t2, get_leaves(t1))
+t1 = deepcopy(trees[1])
+t2 = deepcopy(trees[2])
+MCPhyloTree.number_nodes!(t1)
+MCPhyloTree.number_nodes!(t2)
+MCPhyloTree.set_binary!(t1)
+MCPhyloTree.set_binary!(t2)
+
+non_common_nodes = MCPhyloTree.splitOnCommonEdge(t1, t2)
