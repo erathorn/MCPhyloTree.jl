a314c73a3960503eba7222945cdbf5ab215087f0
Arcumenn <steinar.graessel@gmx.net>
2022-01-14 16:36:03 +0100

    update test file

--- explain.jl
+++ explain.jl
@@ -3,13 +3,21 @@ using Pkg
 Pkg.activate(".")
 using MCPhyloTree
 
-trees = ParseNewick("Drav_mytrees_1.nwk")
-# trees = ParseNewick("gs_tree_JC_20-60-letters.nwk")
+# trees = ParseNewick("Drav_mytrees_1.nwk")
+trees = ParseNewick("gs_tree_JC_20-60-letters.nwk")
 
 t1 = deepcopy(trees[1])
 t2 = deepcopy(trees[2])
 MCPhyloTree.initialize_tree!(t1; height=false)
 MCPhyloTree.initialize_tree!(t2; height=false)
+
+# geo = MCPhyloTree.get_geodesic_nocommon_edges(t1, t2)
+geo = MCPhyloTree.geodesic(t1, t2)
+
+dist = MCPhyloTree.get_distance(geo)
+
+
+
 
 cn = MCPhyloTree.get_common_edges(t1, t2)
 
@@ -20,4 +28,5 @@ internal_nodes1 = filter!(x -> x.nchild != 0, post_order(nc[5][1])[1:end-1])
 internal_nodes2 = filter!(x -> x.nchild != 0, post_order(nc[5][2])[1:end-1])
 mat = MCPhyloTree.get_incidence_matrix(internal_nodes1, internal_nodes2, leaf_dict)
 bg = MCPhyloTree.build_bipartite_graph(mat, [n.inc_length for n in internal_nodes1], [n.inc_length for n in internal_nodes2])
-MCPhyloTree.get_geodesic_nocommon_edges(t1, t2)
+
+
