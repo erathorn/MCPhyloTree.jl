81faf9a22dffb2d4cfe6674cca33b402e162c33b
erathorn <wahle.joh@gmail.com>
2022-06-28 12:23:47 +0200

    fix tests

--- test/moves.jl
+++ test/moves.jl
@@ -41,8 +41,10 @@ end
     @test l == l2
     @test MCPhyloTree.RF_int(tree, tr2)[1] == 2
 
+    ## NNI with root
     @test NNI!(tr2, tr2.num, true) == 0
 
+    ## NNI without target direction
     tr2 = deepcopy(tree)
 
     res = NNI!(tr2, 10)
@@ -53,15 +55,6 @@ end
     @test l == l2
     @test MCPhyloTree.RF_int(tree, tr2)[1] == 2
     
-    tr2 = deepcopy(tree)
-
-    res = NNI!(tr2)
-
-    l = tree_length(tree)
-    l2 = tree_length(tr2)
-
-    @test l == l2
-    @test MCPhyloTree.RF_int(tree, tr2)[1] == 2
 
 end
 
