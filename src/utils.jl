debb8d6cc4ff512ece149fcae127065dd32013a9
erathorn <wahle.joh@gmail.com>
2022-06-28 11:31:25 +0200

    avoid allocations

--- src/utils.jl
+++ src/utils.jl
@@ -5,10 +5,10 @@ Get the longest common prefix.
 """
 function lcp(str1::T, str2::T)::T where T <: AbstractString
     minl = min(length(str1), length(str2))
-    outs::T = ""
+    out_ind = 0
     minl == 0 && return outs
     @inbounds for i in 1:minl
-      str1[i] == str2[i] ? outs *= str1[i] : return outs
+      str1[i] == str2[i] ? out_ind += 1 : break
     end # for
-    return outs
+    return str1[1:out_ind]
 end
